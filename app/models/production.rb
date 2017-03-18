ad4ab827137097d3a5d7edcdc5cbe74995e36c3e
Jonah Roth <jroth@covermymeds.com>
2017-03-18 15:12:04 -0400

    Make sure code is different on each refresh

--- app/models/production.rb
+++ app/models/production.rb
@@ -12,14 +12,14 @@ class Production < ApplicationRecord
 
   def generate_ios_code!
     self.ios_expiration = 7.days.from_now
-    self.ios_code = code_from_string(self.title + self.ios_expiration.to_s)
-    save!
+    self.ios_code = code_from_string(Time.zone.now.to_s + " " + self.title)
+    self.save!
   end
 
   private
 
   def code_from_string(str)
     md5 = Digest::MD5.new
-    md5.hexdigest(str).gsub(/[^\d]/, '')[0..3]
+    md5.hexdigest(str + rand(500).to_s).gsub(/[^\d]/, '')[0..3]
   end
 end
--- spec/models/production_spec.rb
+++ spec/models/production_spec.rb
@@ -1,11 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Production, type: :model do
-  subject { Production.first }
+  subject do
+    Production.create!(title: 'Macbeth', ios_code: '1234',
+                       ios_expiration: 7.days.from_now)
+  end
 
   context '#generate_ios_code!' do
     it 'gives the production a new four digit code' do
-      expect { subject.generate_ios_code! }.to change { subject.ios_code }
+      expect { subject.generate_ios_code! }.to change { subject.reload.ios_code }
     end
 
     it 'sets an expiration date seven days in the future' do
