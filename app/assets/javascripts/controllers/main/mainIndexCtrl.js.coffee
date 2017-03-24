bf4a47d1b31a435fc565df3b09b6a91166b780c0
Jonah Roth <jroth@covermymeds.com>
2017-03-24 14:32:22 -0400

    Better angular setup at static pages

---
+++ app/assets/javascripts/controllers/main/mainIndexCtrl.js.coffee
@@ -0,0 +1,4 @@
+angular.module("prospero", [])
+  .controller 'IndexCtrl', ($scope) ->
+    $scope.title = "My Prospero! "
+    $scope.contents = "Sweet dreams are made of this"
--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,25 @@
 Rails.application.routes.draw do
-  scope(path: '/api') do
-    get  'api/mobile/code'    => 'mobile#code'
-    post 'api/mobile/cell'    => 'mobile#cell'
-    get  'api/mobile/refresh' => 'mobile#refresh'
+  get 'static_pages/index'
+  root 'static_pages#index'
 
-    resources :check_ins
-    resources :crew_calls
-    resources :department_notes
-    resources :departments
-    resources :crew_roles
-    resources :notes
-    resources :rehearsal_scenes
-    resources :rehearsals
-    resources :character_scenes
-    resources :scenes
-    resources :conflicts
-    resources :characters
-    resources :productions
-    resources :people
-    devise_for :users
-    root 'schedules#new'
-    resources :schedules
-  end
+  get  'api/mobile/code'    => 'mobile#code'
+  post 'api/mobile/cell'    => 'mobile#cell'
+  get  'api/mobile/refresh' => 'mobile#refresh'
+
+  resources :check_ins
+  resources :crew_calls
+  resources :department_notes
+  resources :departments
+  resources :crew_roles
+  resources :notes
+  resources :rehearsal_scenes
+  resources :rehearsals
+  resources :character_scenes
+  resources :scenes
+  resources :conflicts
+  resources :characters
+  resources :productions
+  resources :people
+  devise_for :users
+  resources :schedules
 end
