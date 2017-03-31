eb39580180f5ab1246d7433f3448bc758db5919e
Jonah Roth <jroth@covermymeds.com>
2017-03-31 11:23:47 -0400

    Sign in and sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ class ApplicationController < ActionController::Base
 
   rescue_from ActionController::UnknownFormat, with: :deny_access
 
+  before_action :authenticate_user!
+
   # NOTE: http://stackoverflow.com/questions/14734243/rails-csrf-protection-angular-js-protect-from-forgery-makes-me-to-log-out-on
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
