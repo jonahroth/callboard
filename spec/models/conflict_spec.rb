c991049f9f923832261b4f8903aabfd2ea5e521d
Jonah Roth <jroth@covermymeds.com>
2017-04-24 09:38:54 -0400

    Removed all the tests that were failing/outdated

--- spec/models/conflict_spec.rb
+++ spec/models/conflict_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Conflict, type: :model do
+  subject do
+    described_class.create(
+      person: Person.create(first: 'test', last: 'test'),
+      start: 1.day.from_now.to_s,
+      end: 2.days.from_now.to_s
+    )
+  end
+
   it { is_expected.to validate_presence_of(:frequency) }
   it { is_expected.to validate_inclusion_of(:frequency).in_array(%w( O W )) }
 end
