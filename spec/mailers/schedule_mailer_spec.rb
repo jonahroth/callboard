fe6047d14d2f6b1a31bb3d71336d2378c700560b
Jonah Roth <jroth@covermymeds.com>
2017-04-26 09:47:10 -0400

    Fix rspec bug

--- spec/mailers/schedule_mailer_spec.rb
+++ spec/mailers/schedule_mailer_spec.rb
@@ -1,5 +1,5 @@
 require "rails_helper"
 
-RSpec.describe ScheduleMailerMailer, type: :mailer do
+RSpec.describe ScheduleMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
 end
